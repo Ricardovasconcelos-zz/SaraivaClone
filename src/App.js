d9840011c7ff37052f6ee7bbe62c4393c747a106
Ricardovasconcelos <ricardovasconcelos@gmail.com>
2019-07-10 10:42:49 -0300

    feat(config) : adicionando componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import Main from './pages/Main'
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
+        <Header/>
         <Main/>
       </div>
     )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './Header.css'
+import { Navbar } from 'react-bootstrap'
+
+const Header = () => (
+    <div className="header">
+         <Navbar bg="light" variant="light" className="nav">
+            <div className="container">
+                <Navbar.Brand href="#home" className="logo">
+                    <img
+                        alt=""
+                        src="https://image.flaticon.com/icons/png/128/167/167756.png"
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                    />
+                    <h4 className="nav-title">2UBooks</h4>
+                </Navbar.Brand>
+
+                <a href="https://github.com/Ricardovasconcelos"className="gitName">Github.com/Ricardovasconcelos</a>
+                
+            </div>
+        </Navbar>
+
+    </div>
+)
+
+export default Header
