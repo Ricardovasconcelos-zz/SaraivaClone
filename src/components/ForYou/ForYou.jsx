3a593bcace03e4ddb96bab2de7118387de2c6ac6
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-07-17 09:50:18 -0300

    edit grids ForYou component

--- src/components/ForYou/ForYou.jsx
+++ src/components/ForYou/ForYou.jsx
@@ -63,7 +63,7 @@ import { Container, Button } from 'react-bootstrap'
         
         <h2 className="Title-content forYouTitle">Separamos listas com temas especiais para você</h2>
         <div className="ForYou">
-                  <div className="Content-forYou">
+                  <div className="Content-news">
                     <div className="Image">
                       <img
                         src="https://images.livrariasaraiva.com.br/estatico/imagens/list/mais-vendidos-do-new-york-times.jpg"
@@ -75,7 +75,7 @@ import { Container, Button } from 'react-bootstrap'
                     </div>
                   </div>
 
-                  <div className="Content-forYou">
+                  <div className="Content-lowprice">
                     <div className="Image">
                       <img
                         src="https://images.livrariasaraiva.com.br/estatico/2018/tvs/mobile/08-agosto/promocoes/bons-e-baratos/tv_bons_baratos_mb.jpg"
@@ -87,7 +87,7 @@ import { Container, Button } from 'react-bootstrap'
                     </div>
                   </div>
 
-                  <div className="Content-forYou">
+                  <div className="Content-love">
                     <div className="Image">
                       <img
                         src="https://images.livrariasaraiva.com.br/estatico/2018/listas/entre-o-amor-e-a-guerra/tv_entre-o-amor-e-a-guerra_tv_mb.jpg"
@@ -100,7 +100,7 @@ import { Container, Button } from 'react-bootstrap'
                     </div>
                   
      
-                  <div className="Content-forYou">
+                  <div className="Content-invest">
                     <div className="Image">
                       <img
                         src="https://images.livrariasaraiva.com.br/estatico/imagens/list/tv-guia-pratico-investidor_mb.gif"
