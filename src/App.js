52731931e7dc44f3ab8554a4e09530643be1fb63
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2019-09-25 17:51:52 -0300

    update responsive components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
-
+import Header from './components/Header/Header';
+import CarouselComponent from './components/Carousel/CarouselComponent'
+import MostBought from './components/MostBought/MostBought';
+import MoreRead from './components/MoreRead/MoreRead';
+import Favorite from './components/Favorite/Favorite'
+import ForYou from './components/ForYou/ForYou';
+import Frete from './components/Frete/Frete'
+import Payment from './components/Payment/Payment'
+import Footer from './components/Footer/Footer'
 import './App.css'
-import Main from './pages/Main'
+
 
 class App extends Component {
   render() {
     return(
       <div className="App">
-        <Main/>
+        <Header/>
+       <CarouselComponent/>
+       <MostBought/>
+       <Favorite/>
+       <ForYou/>
+        <MoreRead/>
+        <Frete/>
+        <Payment/>
+        <Footer/>
       </div>
     )
   }
